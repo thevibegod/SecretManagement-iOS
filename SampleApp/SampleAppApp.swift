d8f8f6482a9d873ed09bf824364598d6191bce82
Adarsh A <adarsh.a@thoughtworks.com>
2023-03-19 01:28:56 -0700

    Initial Commit

---
+++ SampleApp/SampleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleAppApp.swift
+//  SampleApp
+//
+//  Created by Adarsh A on 18/03/23.
+//
+
+import SwiftUI
+
+@main
+struct SampleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
